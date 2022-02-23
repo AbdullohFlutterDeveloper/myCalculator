ecd71a9449081843078d211d141f298433ec62ea
AbdullohFlutterDeveloper <abdullohabduxalilov21982@gmail.com>
2022-02-23 19:47:15 +0500

    calculator ui is ready

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,6 +1,29 @@
 import 'package:flutter/material.dart';
+import 'package:mycalc/widgets/buttons.dart';
 
 class HomePage extends StatelessWidget {
+  final List<String> buttons = [
+    "C",
+    "DEL",
+    "%",
+    "/",
+    "9",
+    "8",
+    "7",
+    "x",
+    "6",
+    "5",
+    "4",
+    "-",
+    "3",
+    "2",
+    "1",
+    "+",
+    "0",
+    ".",
+    "ANS",
+    "=",
+  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -13,11 +36,49 @@ class HomePage extends StatelessWidget {
           Expanded(
             flex: 2,
             child: Container(
-              color: Colors.deepPurple,
+              color: Colors.purple[100],
+              child: GridView.builder(
+                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 4,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  if (index == 0) {
+                    return MyButton(
+                      buttonText: buttons[index],
+                      color: Colors.green,
+                      textColor: Colors.white,
+                    );
+                  } else if (index == 1) {
+                    return MyButton(
+                      buttonText: buttons[index],
+                      color: Colors.red,
+                      textColor: Colors.white,
+                    );
+                  } else {
+                     return MyButton(
+                    color: isOperator(buttons[index])
+                        ? Colors.deepPurple
+                        : Colors.deepPurple[50],
+                    textColor: isOperator(buttons[index])
+                        ? Colors.white
+                        : Colors.deepPurple,
+                    buttonText: buttons[index],
+                  );
+                  }
+                },
+              ),
             ),
           ),
         ],
       ),
     );
+  }
+
+  bool isOperator(String x) {
+    if (x == "%" || x == "/" || x == "-" || x == "+" || x == "=" || x == "x") {
+      return true;
+    } else {
+      return false;
+    }
   }
 }
--- lib/widgets/buttons.dart
+++ lib/widgets/buttons.dart
@@ -9,15 +9,18 @@ class MyButton extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return ClipRRect(
-      borderRadius: BorderRadius.circular(20),
-      child: Container(
-        color: color,
-        child: Center(
-          child: Text(
-            buttonText!,
-            style: TextStyle(
-              color: textColor,
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(20),
+        child: Container(
+          color: color,
+          child: Center(
+            child: Text(
+              buttonText!,
+              style: TextStyle(
+                color: textColor,
+              ),
             ),
           ),
         ),
