868bdd5aa50a2906b3c6ccb86e2ad3e6abf9ccc9
AbdullohFlutterDeveloper <abdullohabduxalilov21982@gmail.com>
2022-02-23 20:01:54 +0500

    added user input maked buttons clickable

--- lib/screens/home_page.dart
+++ lib/screens/home_page.dart
@@ -1,7 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:mycalc/widgets/buttons.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  var userQuestion = "";
+
+  var userAnswer = "";
+
   final List<String> buttons = [
     "C",
     "DEL",
@@ -24,52 +33,90 @@ class HomePage extends StatelessWidget {
     "ANS",
     "=",
   ];
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[100],
-      body: Column(
-        children: [
-          Expanded(
-            child: Container(),
-          ),
-          Expanded(
-            flex: 2,
-            child: Container(
-              color: Colors.purple[100],
-              child: GridView.builder(
-                gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                  crossAxisCount: 4,
+      body: SafeArea(
+        child: Column(
+          children: [
+            Expanded(
+              child: Container(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    SizedBox(
+                      height: MediaQuery.of(context).size.height / 50,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        alignment: Alignment.centerLeft,
+                        child: Text(
+                          userQuestion,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.all(20.0),
+                      child: Container(
+                        alignment: Alignment.centerRight,
+                        child: Text(
+                          userAnswer,
+                          style: TextStyle(fontSize: 20),
+                        ),
+                      ),
+                    ),
+                  ],
                 ),
-                itemBuilder: (BuildContext context, int index) {
-                  if (index == 0) {
-                    return MyButton(
-                      buttonText: buttons[index],
-                      color: Colors.green,
-                      textColor: Colors.white,
-                    );
-                  } else if (index == 1) {
-                    return MyButton(
-                      buttonText: buttons[index],
-                      color: Colors.red,
-                      textColor: Colors.white,
-                    );
-                  } else {
-                     return MyButton(
-                    color: isOperator(buttons[index])
-                        ? Colors.deepPurple
-                        : Colors.deepPurple[50],
-                    textColor: isOperator(buttons[index])
-                        ? Colors.white
-                        : Colors.deepPurple,
-                    buttonText: buttons[index],
-                  );
-                  }
-                },
               ),
             ),
-          ),
-        ],
+            Expanded(
+              flex: 2,
+              child: Container(
+                color: Colors.purple[100],
+                child: GridView.builder(
+                  gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                  ),
+                  itemCount: buttons.length,
+                  itemBuilder: (BuildContext context, int index) {
+                    if (index == 0) {
+                      return MyButton(
+                        buttonText: buttons[index],
+                        color: Colors.green,
+                        textColor: Colors.white,
+                      );
+                    } else if (index == 1) {
+                      return MyButton(
+                        buttonText: buttons[index],
+                        color: Colors.red,
+                        textColor: Colors.white,
+                      );
+                    } else {
+                      return MyButton(
+                        buttonTapped: () {
+                          setState(() {
+                            userQuestion += buttons[index];
+                          });
+                        },
+                        color: isOperator(buttons[index])
+                            ? Colors.deepPurple
+                            : Colors.deepPurple[50],
+                        textColor: isOperator(buttons[index])
+                            ? Colors.white
+                            : Colors.deepPurple,
+                        buttonText: buttons[index],
+                      );
+                    }
+                  },
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
